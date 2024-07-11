9b00ace8e7df1a10809fd2cbb62e4c246b60757f
Jon Ranes <jon.ranes@upsilonpayments.com>
2024-07-11 10:56:47 -0700

    setting up auth

---
+++ PayItGlobal.App/ConfigurationModels/ApiSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayItGlobal.App.ConfigurationModels
+{
+    public class ApiSettings
+    {
+        public string BaseUrl { get; set; }
+        public string LoginEndpoint { get; set; }
+        public string RefreshTokenEndpoint { get; set; }
+    }
+}
