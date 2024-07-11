341183edc76d6e20c0194f9a1da9ec6336d35c12
Jon Ranes <jon.ranes@upsilonpayments.com>
2024-07-10 20:25:06 -0700

    temp checkin

--- PayItGlobal.App/AppShell.cs
+++ PayItGlobal.App/AppShell.cs
@@ -1,28 +1,68 @@
 ﻿using MauiReactor;
+using MauiReactor.Canvas;
 using PayItGlobal.App.Pages;
+using PayItGlobal.App.Resources.Styles;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PayItGlobal.App
 {
     internal class AppShell : Component
     {
-        public override VisualNode Render()
-            => Shell(
-                FlyoutItem("HomePage",
-                    ShellContent()
-                        .Title("HomePage")
-                        .RenderContent(() => new HomePage())
-                ),
-                FlyoutItem("LandingPage",
-                    ShellContent()
-                        .Title("LandingPage")
-                        .RenderContent(() => new LandingPage())
-                )
-            );
-    }
+        private PageEnum _currentPage = PageEnum.HomePage; // Initial page
 
+        public override VisualNode Render()
+        {
+            return new VStack
+            {
+                // Your pages or content here based on _currentPage
+                RenderPageContent(),
+
+                // Custom tab bar at the bottom
+                RenderCustomTabBar()
+            };
+        }
+
+        private VisualNode RenderPageContent()
+        {
+            // Switch between pages based on _currentPage
+            switch (_currentPage)
+            {
+                case PageEnum.HomePage:
+                    return new HomePage();
+                case PageEnum.EventsPage:
+                    return new EventsPage();
+                default:
+                    return new NotFoundPage();
+            }
+        }
+
+        private VisualNode RenderCustomTabBar()
+        {
+            ImageButton createButton(PageEnum page, int column) =>
+                new ImageButton()
+                    .Aspect(Aspect.Center)
+                    .Source(() => _currentPage != page ? $"{page.ToString().ToLowerInvariant()}.png" : $"{page.ToString().ToLowerInvariant()}_on.png")
+                    .GridColumn(column)
+                    .OnClicked(() => UpdateCurrentPage(page))
+                    .Margin(_currentPage == page ? new Thickness(0, 0, 0, 10) : new Thickness())
+                    .WithAnimation(Easing.BounceOut, 300);
+
+            return new Grid("*", "*")
+            {
+                // Your Grid implementation
+            }
+            .VEnd()
+            .HeightRequest(92);
+        }
+
+        private void UpdateCurrentPage(PageEnum page)
+        {
+            if (_currentPage != page)
+            {
+                _currentPage = page;
+                Invalidate(); // Trigger a re-render of the component
+            }
+        }
+    }
 }
--- PayItGlobal.App/Pages/MainPage.cs
+++ PayItGlobal.App/Pages/MainPage.cs
@@ -42,89 +42,25 @@ namespace PayItGlobal.App.Pages
         public override VisualNode Render()
         {
             return new NavigationPage
-        {
-            new ContentPage
             {
-                new Grid("*", "*")
+                new ContentPage
                 {
-                    RenderPage(),
+                    new Grid("*", "*")
+                    {
+                        RenderPage(),
 
-                    RenderTabBar()
+                        // RenderTabBar() call removed
+                    }
                 }
-            }
-            .Set(MauiControls.NavigationPage.HasNavigationBarProperty, false)
-            .BackgroundColor(ThemeBrushes.Background)
-        };
+                .Set(MauiControls.NavigationPage.HasNavigationBarProperty, false)
+                .BackgroundColor(ThemeBrushes.Background)
+            };
         }
         VisualNode RenderPage() => State.CurrentPage switch
         {
             PageEnum.HomePage => new HomePage(),
             _ => throw new NotImplementedException(),
         };
-        VisualNode RenderTabBar()
-        {
-            ImageButton createButton(PageEnum page, int column) =>
-                new ImageButton()
-                    .Aspect(Aspect.Center)
-                    .Source(() => State.CurrentPage != page ? $"{page.ToString().ToLowerInvariant()}.png" : $"{page.ToString().ToLowerInvariant()}_on.png")
-                    .GridColumn(column)
-                    .OnClicked(() => SetState(s => s.CurrentPage = page))
-                    .Margin(State.CurrentPage == page ? new Thickness(0, 0, 0, 10) : new Thickness())
-                    .WithAnimation(Easing.BounceOut, 300)
-                    ;
-
-            return new Grid("*", "*")
-        {
-            new CanvasView()
-            {
-                new DropShadow
-                {
-                    new Box()
-                        .Margin(0,20,0,0)
-                        .CornerRadius(24,24,0,0)
-                        .BackgroundColor (ThemeBrushes.White)
-                }
-                .Color(ThemeBrushes.DarkShadow)
-                .Size(0, -8)
-                .Blur(32),
-
-                new Row("* * * * *")
-                {
-                    Enum.GetValues(typeof(PageEnum))
-                        .Cast<PageEnum>()
-                        .Select(page =>
-                            new Align()
-                            {
-                                new Ellipse()
-                                    .FillColor(ThemeBrushes.Purple10)
-                            }
-                            .IsVisible(State.CurrentPage == page)
-                            .Width(4)
-                            .Height(4)
-                            .VEnd()
-                            .HCenter()
-                            .Margin(16)
-                        )
-                        .ToArray()
-                }
-
-            }
-            .BackgroundColor(Colors.Transparent)
-            .GridRow(1),
-
-            new Grid("*", "* * * * *")
-            {
-                createButton(PageEnum.HomePage, 0),
-                createButton(PageEnum.EventsPage, 1),
-                createButton(PageEnum.CommunityPage, 2),
-                createButton(PageEnum.AssetsPage, 3),
-                createButton(PageEnum.CalendarPage, 4)
-            }
-            .Padding(0,20,0,0)
-        }
-            .VEnd()
-            .HeightRequest(92);
-        }
+        // RenderTabBar method removed
     }
-
 }
